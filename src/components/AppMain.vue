dc39b868d207fc1fc5c0c630efbc33e142c3e04f
Leo9723 <pala9761@gmail.com>
2023-01-12 15:07:20 +0100

    aggiunta elementi

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,20 @@
 <script>
+import AppList from './card.vue'
+
 export default {
-  
+  components: {
+    AppList
+  },
+  data(){
+    return {
+      
+    }
+  }
 }
 </script>
 <template lang="">
   <div class="maincont">
-    Content goes here 
+    <AppList v-for="(card, index) in AppList" :img="card.thumb" :title="card.series">
   </div>
 </template>
 <style lang="scss">
